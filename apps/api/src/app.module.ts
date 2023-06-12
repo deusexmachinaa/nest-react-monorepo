d14aec05067aa872f4471c46c1f7548e020fea6d
deusexmachinaa <gjfzml@gmail.com>
2023-06-12 19:47:40 +0900

    위치변경

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { AppController } from './app.controller';
+import { AppController } from './controllers/app.controller';
 import { AppService } from './app.service';
 import { HttpModule } from '@nestjs/axios';
 
